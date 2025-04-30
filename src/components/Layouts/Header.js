d9a4ac63700f2bc3555ace53cf114f8990d5106c
Wasiullah1 <waseeullah14@gmail.com>
2025-04-30 18:06:41 +0500

    Food Delivery App ReactJS

---
+++ src/components/Layouts/Header.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import {Container, Nav, Navbar} from 'react-bootstrap';
+import '../../styles/HeaderStyle.css'
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/logo/logo.png'
+
+function Header() {
+  return (
+      <>
+      <Navbar collapseOnSelect expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">
+          <Link>
+          <img src={Logo} alt='Logo' className='img-fluid' />
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+            <Nav.Link as={Link} to="/menu">Our Menu</Nav.Link>
+            <Nav.Link as={Link} to="/shop">Shop</Nav.Link>
+            <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+          </Nav>
+          
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+      </>
+    )
+}
+
+export default Header
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Container, Nav, Navbar} from 'react-bootstrap';
+import '../../styles/HeaderStyle.css'
+import Layout from '../../components/Layouts/Layout';
+
+function Home() {
+  return (
+    <>
+    <Layout>
+        <div>Home</div>
+    </Layout>
+    </>
+  )
+}
+
+export default Home
